ac67c49032160b9b5494168f98107596d7db9f6e
zephinzer <govtech@joeir.net>
2018-02-15 11:57:47 +0800

    added assertions to test script

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e;
 CURR_DIR="$(dirname $0)";
 printf "Testing NPM...\n";
 
@@ -8,8 +9,13 @@ printf -- "NPM_ALL: ${NPM_ALL}\n";
 NPM_PROD="$(${CURR_DIR}/../dephash.sh ${CURR_DIR}/npm/package.json --npm --prod)";
 printf -- "NPM_PROD: ${NPM_PROD}\n";
 
+if [ "${NPM_ALL}" = "${NPM_PROD}" ]; then exit 1;
+
 NPM_DEV="$(${CURR_DIR}/../dephash.sh ${CURR_DIR}/npm/package.json --npm --dev)";
 printf -- "NPM_DEV: ${NPM_DEV}\n";
+
+if [ "${NPM_PROD}" = "${NPM_DEV}" ]; then exit 1;
+if [ "${NPM_PROD}" = "${NPM_ALL}" ]; then exit 1;
 
 YARN_ALL="$(${CURR_DIR}/../dephash.sh ${CURR_DIR}/yarn/yarn.lock --yarn)";
 printf -- "YARN_ALL: ${YARN_ALL}\n";
