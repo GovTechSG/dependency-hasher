a35ece7493fcf4d31d72f1f2489e8b9346e6e300
zephinzer <govtech@joeir.net>
2018-02-15 12:21:39 +0800

    added release stage and fixed missing fi

--- test/run.sh
+++ test/run.sh
@@ -9,7 +9,7 @@ printf -- "NPM_ALL: ${NPM_ALL}\n";
 NPM_PROD="$(${CURR_DIR}/../dephash ${CURR_DIR}/npm/package.json --npm --prod)";
 printf -- "NPM_PROD: ${NPM_PROD}\n";
 
-if [ "${NPM_ALL}" = "${NPM_PROD}" ]; then exit 1;
+if [ "${NPM_ALL}" = "${NPM_PROD}" ]; then exit 1; fi;
 
 NPM_DEV="$(${CURR_DIR}/../dephash ${CURR_DIR}/npm/package.json --npm --dev)";
 printf -- "NPM_DEV: ${NPM_DEV}\n";
